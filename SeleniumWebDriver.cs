cd8b61df4ef30c67342370b795a7c34452885783
Teodora217 <teodoraivanova007@abv.bg>
2023-03-10 17:26:51 +0200

    Add project files.

---
+++ SeleniumWebDriver.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriverDemo
+{
+    public class SeleniumWebDriver
+    {
+        static void Main()
+        {
+            // create browser instance
+            var driver = new ChromeDriver();
+
+            //navigate to Wikipedia
+            driver.Url = "https://www.wikipedia.org/";
+
+            // Get browser title
+            var pageTitle = driver.Title;
+
+            Console.WriteLine("The page title is " + pageTitle);
+
+            if(pageTitle == "Wikipedia")
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            // close browser 
+            driver.Quit();
+        }
+    }
+}
